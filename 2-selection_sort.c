e155eaad749b901d165a677155129c32280b982f
kafilat1210 <cafylhearty@gmail.com>
2024-02-14 06:33:04 -0800

    select algo

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			/* Swap the elements */
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			/* Print the array after each swap */
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -16,7 +16,7 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
-
+void selection_sort(int *array, size_t size);
 /* Prototype of bubble sort function */
 void bubble_sort(int *array, size_t size);
 
